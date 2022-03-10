ade03e82470a406a61c0583d9bde5aae11de91ec
RomanJack7 <sainathmnkr@gmail.com>
2022-03-09 21:22:54 -0500

    First Commit stable build

--- t3.py
+++ t3.py
@@ -33,7 +33,7 @@ df = df.set_index('Date')
 df = df['2013-01-01':'2017-12-31'].resample('W').sum()
 df.head()  
 #st.sidebar.selectbox("Choose the forecasting method to continue", ['Simple Exponential Smoothing','HOLTs Forecasting Method', 'HOLT-WINTERs Forecasting Method'])
-rad = st.sidebar.radio("Navigation - Choose a forecasting method to continue",["Visualising Your Data","Simple Exponential Smoothing","HOLTs Forecasting Method","HOLT-WINTERs Forecasting Method","Sarima"])
+rad = st.sidebar.radio("Navigation - Choose a forecasting method to continue",["Visualising Your Data","Simple Exponential Smoothing","HOLTs Forecasting Method","HOLT-WINTERs Forecasting Method"])#"Sarima"])
 
 if rad == "Visualising Your Data":
     header = st.container() 
@@ -306,147 +306,149 @@ else:
 
 ###############################################################################
 
-if rad =="Sarima":
 # =============================================================================
-#     progress = st.progress(0)
-#     for i in range(100):
-#         time.sleep(0.1)
-#         progress.progress(i+1)
+# if rad =="Sarima":
+# # =============================================================================
+# #     progress = st.progress(0)
+# #     for i in range(100):
+# #         time.sleep(0.1)
+# #         progress.progress(i+1)
+# # =============================================================================
+#         
+#     import itertools
+#     
+#     def sarima_grid_search(y,seasonal_period):
+#         p = d = q = range(0, 2)
+#         pdq = list(itertools.product(p, d, q))
+#         seasonal_pdq = [(x[0], x[1], x[2],seasonal_period) for x in list(itertools.product(p, d, q))]
+#     
+#         mini = float('+inf')
+#     
+#     
+#         for param in pdq:
+#             for param_seasonal in seasonal_pdq:
+#                 try:
+#                     mod = sm.tsa.statespace.SARIMAX(y,
+#                                                 order=param,
+#                                                 seasonal_order=param_seasonal,
+#                                                 enforce_stationarity=False,
+#                                                 enforce_invertibility=False)
+# 
+#                     results = mod.fit()
+#                 
+#                     if results.aic < mini:
+#                         mini = results.aic
+#                         param_mini = param
+#                         param_seasonal_mini = param_seasonal
+# 
+# #                   print('SARIMA{}x{} - AIC:{}'.format(param, param_seasonal, results.aic))
+#                 except:
+#                     continue
+#                 print('The set of parameters with the minimum AIC is: SARIMA{}x{} - AIC:{}'.format(param_mini, param_seasonal_mini, mini))
+#         
+#     sarima_grid_search(y,52)
+#     
+#     # Call this function after pick the right(p,d,q) for SARIMA based on AIC               
+#     def sarima_eva(y,order,seasonal_order,seasonal_period,pred_date,y_to_test):
+#         # fit the model 
+#         mod = sm.tsa.statespace.SARIMAX(y,
+#                                     order=order,
+#                                     seasonal_order=seasonal_order,
+#                                     enforce_stationarity=False,
+#                                     enforce_invertibility=False)
+#     
+#         results = mod.fit()
+#         print(results.summary().tables[1])
+#         
+#         results.plot_diagnostics(figsize=(16, 8))
+#         plt.show()
+#         
+#         # The dynamic=False argument ensures that we produce one-step ahead forecasts, 
+#         # meaning that forecasts at each point are generated using the full history up to that point.
+#         pred = results.get_prediction(start=pd.to_datetime(pred_date), dynamic=False)
+#         pred_ci = pred.conf_int()
+#         y_forecasted = pred.predicted_mean
+#         mse = ((y_forecasted - y_to_test) ** 2).mean()
+#         print('The Root Mean Squared Error of SARIMA with season_length={} and dynamic = False {}'.format(seasonal_period,round(np.sqrt(mse), 2)))
+#     
+#         ax = y.plot(label='observed')
+#         y_forecasted.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+#         ax.fill_between(pred_ci.index,
+#                         pred_ci.iloc[:, 0],
+#                         pred_ci.iloc[:, 1], color='k', alpha=.2)
+#     
+#         ax.set_xlabel('Date')
+#         ax.set_ylabel('Sessions')
+#         plt.legend()
+#         plt.show()
+#     
+#         # A better representation of our true predictive power can be obtained using dynamic forecasts. 
+#         # In this case, we only use information from the time series up to a certain point, 
+#         # and after that, forecasts are generated using values from previous forecasted time points.
+#         pred_dynamic = results.get_prediction(start=pd.to_datetime(pred_date), dynamic=True, full_results=True)
+#         pred_dynamic_ci = pred_dynamic.conf_int()
+#         y_forecasted_dynamic = pred_dynamic.predicted_mean
+#         mse_dynamic = ((y_forecasted_dynamic - y_to_test) ** 2).mean()
+#         print('The Root Mean Squared Error of SARIMA with season_length={} and dynamic = True {}'.format(seasonal_period,round(np.sqrt(mse_dynamic), 2)))
+#     
+#         ax = y.plot(label='observed')
+#         y_forecasted_dynamic.plot(label='Dynamic Forecast', ax=ax,figsize=(14, 7))
+#         ax.fill_between(pred_dynamic_ci.index,
+#                         pred_dynamic_ci.iloc[:, 0],
+#                         pred_dynamic_ci.iloc[:, 1], color='k', alpha=.2)
+#     
+#         ax.set_xlabel('Date')
+#         ax.set_ylabel('Sessions')
+#     
+#         plt.legend()
+#         plt.show()
+#         
+#         return (results)
+# 
+#     model = sarima_eva(y,(1, 0, 1),(0, 1, 1, 52),52,'2016-07-03',y_to_val)
+#     plt.close(fig)
+#     
+#     def forecast(model,predict_steps,y):
+#     
+#         pred_uc = model.get_forecast(steps=predict_steps)
+#     
+#         #SARIMAXResults.conf_int, can change alpha,the default alpha = .05 returns a 95% confidence interval.
+#         pred_ci = pred_uc.conf_int()
+#     
+#         ax = y.plot(label='observed', figsize=(14, 7))
+#     #     print(pred_uc.predicted_mean)
+#         pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+#         ax.fill_between(pred_ci.index,
+#                         pred_ci.iloc[:, 0],
+#                         pred_ci.iloc[:, 1], color='k', alpha=.25)
+#         ax.set_xlabel('Date')
+#         ax.set_ylabel(y.name)
+#     
+#         plt.legend()
+#         plt.show()
+#         #ax.figure.savefig('sarima.png')
+#         
+#         # Produce the forcasted tables 
+#         pm = pred_uc.predicted_mean.reset_index()
+#         pm.columns = ['Date','Predicted_Mean']
+#         pci = pred_ci.reset_index()
+#         pci.columns = ['Date','Lower Bound','Upper Bound']
+#         final_table = pm.join(pci.set_index('Date'), on='Date')
+#         
+#         return (final_table)    
+#     
+#     final_table = forecast(model,15,y)
+#     final_table.head()
+#     #ax.figure.savefig(final_table)
+#         
+#     
+#     sarima_sl = st.container()
+#     with sarima_sl:
+#         st.header("SARIMA")
+#         st.text("SARIMA stands for Seasonal Autoregressive Integrated Moving Average,")
+#         st.text("which is accurate and gives a range of probable forecast")
+#         st.table(final_table)
 # =============================================================================
-        
-    import itertools
-    
-    def sarima_grid_search(y,seasonal_period):
-        p = d = q = range(0, 2)
-        pdq = list(itertools.product(p, d, q))
-        seasonal_pdq = [(x[0], x[1], x[2],seasonal_period) for x in list(itertools.product(p, d, q))]
-    
-        mini = float('+inf')
-    
-    
-        for param in pdq:
-            for param_seasonal in seasonal_pdq:
-                try:
-                    mod = sm.tsa.statespace.SARIMAX(y,
-                                                order=param,
-                                                seasonal_order=param_seasonal,
-                                                enforce_stationarity=False,
-                                                enforce_invertibility=False)
-
-                    results = mod.fit()
-                
-                    if results.aic < mini:
-                        mini = results.aic
-                        param_mini = param
-                        param_seasonal_mini = param_seasonal
-
-#                   print('SARIMA{}x{} - AIC:{}'.format(param, param_seasonal, results.aic))
-                except:
-                    continue
-                print('The set of parameters with the minimum AIC is: SARIMA{}x{} - AIC:{}'.format(param_mini, param_seasonal_mini, mini))
-        
-    sarima_grid_search(y,52)
-    
-    # Call this function after pick the right(p,d,q) for SARIMA based on AIC               
-    def sarima_eva(y,order,seasonal_order,seasonal_period,pred_date,y_to_test):
-        # fit the model 
-        mod = sm.tsa.statespace.SARIMAX(y,
-                                    order=order,
-                                    seasonal_order=seasonal_order,
-                                    enforce_stationarity=False,
-                                    enforce_invertibility=False)
-    
-        results = mod.fit()
-        print(results.summary().tables[1])
-        
-        results.plot_diagnostics(figsize=(16, 8))
-        plt.show()
-        
-        # The dynamic=False argument ensures that we produce one-step ahead forecasts, 
-        # meaning that forecasts at each point are generated using the full history up to that point.
-        pred = results.get_prediction(start=pd.to_datetime(pred_date), dynamic=False)
-        pred_ci = pred.conf_int()
-        y_forecasted = pred.predicted_mean
-        mse = ((y_forecasted - y_to_test) ** 2).mean()
-        print('The Root Mean Squared Error of SARIMA with season_length={} and dynamic = False {}'.format(seasonal_period,round(np.sqrt(mse), 2)))
-    
-        ax = y.plot(label='observed')
-        y_forecasted.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
-        ax.fill_between(pred_ci.index,
-                        pred_ci.iloc[:, 0],
-                        pred_ci.iloc[:, 1], color='k', alpha=.2)
-    
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Sessions')
-        plt.legend()
-        plt.show()
-    
-        # A better representation of our true predictive power can be obtained using dynamic forecasts. 
-        # In this case, we only use information from the time series up to a certain point, 
-        # and after that, forecasts are generated using values from previous forecasted time points.
-        pred_dynamic = results.get_prediction(start=pd.to_datetime(pred_date), dynamic=True, full_results=True)
-        pred_dynamic_ci = pred_dynamic.conf_int()
-        y_forecasted_dynamic = pred_dynamic.predicted_mean
-        mse_dynamic = ((y_forecasted_dynamic - y_to_test) ** 2).mean()
-        print('The Root Mean Squared Error of SARIMA with season_length={} and dynamic = True {}'.format(seasonal_period,round(np.sqrt(mse_dynamic), 2)))
-    
-        ax = y.plot(label='observed')
-        y_forecasted_dynamic.plot(label='Dynamic Forecast', ax=ax,figsize=(14, 7))
-        ax.fill_between(pred_dynamic_ci.index,
-                        pred_dynamic_ci.iloc[:, 0],
-                        pred_dynamic_ci.iloc[:, 1], color='k', alpha=.2)
-    
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Sessions')
-    
-        plt.legend()
-        plt.show()
-        
-        return (results)
-
-    model = sarima_eva(y,(1, 0, 1),(0, 1, 1, 52),52,'2016-07-03',y_to_val)
-    plt.close(fig)
-    
-    def forecast(model,predict_steps,y):
-    
-        pred_uc = model.get_forecast(steps=predict_steps)
-    
-        #SARIMAXResults.conf_int, can change alpha,the default alpha = .05 returns a 95% confidence interval.
-        pred_ci = pred_uc.conf_int()
-    
-        ax = y.plot(label='observed', figsize=(14, 7))
-    #     print(pred_uc.predicted_mean)
-        pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
-        ax.fill_between(pred_ci.index,
-                        pred_ci.iloc[:, 0],
-                        pred_ci.iloc[:, 1], color='k', alpha=.25)
-        ax.set_xlabel('Date')
-        ax.set_ylabel(y.name)
-    
-        plt.legend()
-        plt.show()
-        #ax.figure.savefig('sarima.png')
-        
-        # Produce the forcasted tables 
-        pm = pred_uc.predicted_mean.reset_index()
-        pm.columns = ['Date','Predicted_Mean']
-        pci = pred_ci.reset_index()
-        pci.columns = ['Date','Lower Bound','Upper Bound']
-        final_table = pm.join(pci.set_index('Date'), on='Date')
-        
-        return (final_table)    
-    
-    final_table = forecast(model,15,y)
-    final_table.head()
-    #ax.figure.savefig(final_table)
-        
-    
-    sarima_sl = st.container()
-    with sarima_sl:
-        st.header("SARIMA")
-        st.text("SARIMA stands for Seasonal Autoregressive Integrated Moving Average,")
-        st.text("which is accurate and gives a range of probable forecast")
-        st.table(final_table)
 
 ###############################################################################
 
